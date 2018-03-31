ac0619513e04c0b74848db9c1edb095c615e5b05
jinghao Xie <a0978171892@gmail.com>
2018-03-31 22:31:27 +0800

    SelectList add

--- MVC_HW/Controllers/SalesOrdersController.cs
+++ MVC_HW/Controllers/SalesOrdersController.cs
@@ -11,20 +11,32 @@ namespace MVC_HW.Controllers
     public class SalesOrdersController : Controller
     {
         public static List<Models.Order> OrderList;
+        public static List<Models.Employees> EmployeesList;
+        public static List<Models.Shippers> ShippersList;
         // GET: SalesOrders
         public ActionResult Index()
         {
             
             OrderList = new Models.Order().Initialize();
 
-          
+           
             
             return View(OrderList);
         }
-        public ActionResult search()
+        public ActionResult Search()
         {
-            
-
+            EmployeesList = new Models.Employees().Initialize();
+            ShippersList = new Models.Shippers().Initialize();
+            List<SelectListItem> selectlistitem=new List<SelectListItem>();
+            foreach (var x in EmployeesList) {
+                selectlistitem.Add(new SelectListItem()
+                {
+                    Text =x.FirstName+x.LastName,
+                    Value=x.EmployeeID.ToString()
+                });
+            }
+            SelectList SL = new SelectList(selectlistitem);
+            ViewBag.SL = SL.Items;
             return View();
         }
         [HttpPost]
