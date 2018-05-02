9cb525453a69839a86e8c8914821f7e31a096b83
jinghao Xie <a0978171892@gmail.com>
2018-05-02 15:12:13 +0800

    add sqlconnection object in basedao

--- MVC_HW/Dao/OrderDao.cs
+++ MVC_HW/Dao/OrderDao.cs
@@ -12,8 +12,8 @@ namespace MVC_HW.Dao
     public class OrderDao:BaseDao
     {
         public DataSet GetDataSet() {
-            String ConnStr = GetString();
-            SqlConnection conn = new SqlConnection(ConnStr);
+            
+            SqlConnection conn = GetSqlConnection();
             String sql = "select d.CompanyName as Cuscom,b.CompanyName as shipcom,* from [Sales].Orders a left join Sales.Shippers b on a.ShipperID=b.ShipperID left join HR.Employees c on a.EmployeeID=c.EmployeeID left join Sales.Customers d on a.CustomerID=d.CustomerID";
             SqlDataAdapter da = new SqlDataAdapter(sql, conn);
             DataSet ds = new DataSet();
